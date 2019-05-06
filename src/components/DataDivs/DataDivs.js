788c93be14cdd51fa376e02a8fa9c2603dc13376
Matthew Tetrault <mktetrault@yahoo.com>
2019-05-06 12:42:10 -0400

    Update DataDivs.js

--- src/components/DataDivs/DataDivs.js
+++ src/components/DataDivs/DataDivs.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import bitcoin from './images/bitcoin.ico'
+import litecoin from './images/litecoin.ico'
+import ethereum from './images/ethereum.ico'
 import './DataDivs.css';
 
 const dataDivs = props => (
@@ -10,17 +13,17 @@ const dataDivs = props => (
         </div>
         <div className="list-body">
             <div className="table-row">
-                <div className="row-name"><img src={'/images/bitcoin.ico'} alt="Bitcoin Icon" />Bitcoin</div>
+                <div className="row-name"><img src={bitcoin} alt="Bitcoin Icon" />Bitcoin</div>
                 <div className="row-amount">1.0</div>
                 <div className="row-value">$1,000</div>
             </div>
             <div className="table-row">
-                <div className="row-name"><img src={'/images/litecoin.ico'} alt="Litecoin Icon" />Litecoin</div>
+                <div className="row-name"><img src={litecoin} alt="Litecoin Icon" />Litecoin</div>
                 <div className="row-amount">2.0</div>
                 <div className="row-value">$2,000</div>
             </div>
             <div className="table-row">
-                <div className="row-name"><img src={'/images/ethereum.ico'} alt="Ethereum Icon" />Ethereum</div>
+                <div className="row-name"><img src={ethereum} alt="Ethereum Icon" />Ethereum</div>
                 <div className="row-amount">3.0</div>
                 <div className="row-value">$3,000</div>
             </div>
