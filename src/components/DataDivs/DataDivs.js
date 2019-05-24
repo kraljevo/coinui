b2c86bd15e5fba1e2bddd7ecf11d4511ebb94e9a
Matthew Tetrault <mktetrault@yahoo.com>
2019-05-24 10:18:18 -0400

    reorganizing data

--- src/components/DataDivs/DataDivs.js
+++ src/components/DataDivs/DataDivs.js
@@ -5,12 +5,21 @@ var NumberFormat = require('react-number-format');
 const DataDivs = props => {
   return (
     <div className="coin-list">
-      {Object.keys(props.coins).map((key) => (
-        <div id="crypto-container">
-          <div className="favorites-sym"><img src={'https://www.cryptocompare.com' + props.coins[key].USD.IMAGEURL} alt={`${key} icon`} />{key}</div>
-          <div className="favorites-price"><NumberFormat value={props.coins[key].USD.PRICE} displayType={'text'} decimalprecision={2} thousandSeparator={true} prefix={'$'} /></div>
-        </div>
-      ))}
+      <div className="list-body">
+        {Object.keys(props.coins).map((key) => (
+          <div className="crypto-container">
+            <div className="favorites-img">
+              <img src={'https://www.cryptocompare.com' + props.coins[key].USD.IMAGEURL} alt={`${key} icon`} />
+            </div>
+            <div className="favorites-sym">
+              {key}
+            </div>
+            <div className="favorites-price">
+              <NumberFormat value={props.coins[key].USD.PRICE} displayType={'text'} decimalprecision={2} thousandSeparator={true} prefix={'$'} />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
