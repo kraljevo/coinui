b5287e554a1bf1e4e17655d94255af3e9129c9d6
Matthew Tetrault <mktetrault@yahoo.com>
2019-07-04 08:39:52 -0400

    build/deploy

---
+++ build/precache-manifest.0d57cbcff8069d0d5f9f7142c5f4df2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c398f65e688afb5610f7b1d168ff5a3",
+    "url": "/coinui/index.html"
+  },
+  {
+    "revision": "657c64cd0709d4f0412d",
+    "url": "/coinui/static/css/main.e3551084.chunk.css"
+  },
+  {
+    "revision": "07540ca746fc4964e308",
+    "url": "/coinui/static/js/2.29846a1c.chunk.js"
+  },
+  {
+    "revision": "657c64cd0709d4f0412d",
+    "url": "/coinui/static/js/main.a94c0a92.chunk.js"
+  },
+  {
+    "revision": "94ee1c98c221f4991df2",
+    "url": "/coinui/static/js/runtime~main.1ab53f73.js"
+  }
+]);
